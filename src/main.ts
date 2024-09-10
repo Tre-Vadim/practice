b5f5d89c805834455aec0b5ccc340f2dae60fa4e
Vadim Goloviychuk <vadim.goloviychuk@gmail.com>
2024-09-10 14:00:12 +0300

    the deployment was added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import './style.css'
-import * as THREE from 'three'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import Stats from 'three/addons/libs/stats.module.js';
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
 
@@ -22,13 +25,31 @@ const material = new THREE.MeshNormalMaterial({ wireframe: true })
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
+new OrbitControls(camera, renderer.domElement);
+
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+
+const gui = new GUI();
+const cubeFolder = gui.addFolder('Cube');
+cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2);
+cubeFolder.open();
+
+const cameraFolder = gui.addFolder('Camera');
+cameraFolder.add(camera.position, 'z', 0, 20);
+
 function animate() {
   requestAnimationFrame(animate)
 
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+  // stats.begin();
+  // cube.rotation.x += 0.01
+  // cube.rotation.y += 0.01
+  // stats.end();
 
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
+  stats.update();
 }
 
 animate()
