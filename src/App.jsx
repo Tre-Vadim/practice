9e917fcb8be6635d3a2e13ba083d5bca7d4ebcc6
Goloviychuk Vadim <vadim.goloviychuk@gmail.com>
2024-10-14 16:21:58 +0300

    work with refs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber';
 
-import { Box } from './Box'
+import { Box } from './Box';
 
 const App = () => (
   <Canvas camera={{ position: [0, 0, 2] }}>
     <Box position={[-0.75, 0, 0]} name="A" />
     <Box position={[0.75, 0, 0]} name="B" />
   </Canvas>
-)
+);
 
-export default App
+export default App;
--- src/Box.jsx
+++ src/Box.jsx
@@ -1,6 +1,14 @@
-export const Box = (props) => (
-  <mesh {...props}>
-    <boxGeometry />
-    <meshBasicMaterial color={0x00ff00} wireframe />
-  </mesh>
-)
+import { useRef } from 'react';
+
+export const Box = (props) => {
+  const instanceRef = useRef(null);
+  const materialRef = useRef(null);
+  console.log('instanceRef', instanceRef);
+  console.log('materialRef', materialRef);
+  return (
+    <mesh ref={instanceRef} {...props}>
+      <boxGeometry />
+      <meshBasicMaterial ref={materialRef} color={0x00ff00} wireframe />
+    </mesh>
+  );
+};
