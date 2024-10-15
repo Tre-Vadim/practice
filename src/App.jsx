a7184fdfd19584542a3268942eac3d784d4e7241
Goloviychuk Vadim <vadim.goloviychuk@gmail.com>
2024-10-15 13:54:12 +0300

    work with the sharing data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { Canvas } from '@react-three/fiber';
+import * as THREE from 'three';
 
-import { Box } from './Box';
+import { Polyhedron } from './Polyhedron';
 
-const App = () => (
-  <Canvas camera={{ position: [0, 0, 2] }}>
-    <Box position={[-0.75, 0, 0]} name="A" />
-    <Box position={[0.75, 0, 0]} name="B" />
-  </Canvas>
-);
+const App = () => {
+  const polyhedron = [
+    new THREE.BoxGeometry(),
+    new THREE.SphereGeometry(0.785398),
+    new THREE.DodecahedronGeometry(0.785398)
+  ];
+
+  return (
+    <Canvas camera={{ position: [0, 0, 3] }}>
+      <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
+    </Canvas>
+  );
+};
 
 export default App;
