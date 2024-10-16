b43e935a4383267c414702fb0577ae6444a1c387
Goloviychuk Vadim <vadim.goloviychuk@gmail.com>
2024-10-16 13:08:22 +0300

    work with the Environment Map

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-import { Stats, OrbitControls, Circle } from '@react-three/drei';
+import { Stats, OrbitControls, Environment } from '@react-three/drei';
 import { Canvas, useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
 
 export default function App() {
   const gltf = useLoader(GLTFLoader, '/models/monkey.glb');
-  console.log('gltf', gltf);
 
   return (
-    <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
-      <directionalLight
-        position={[3.3, 1.0, 4.4]}
-        castShadow
-        intensity={Math.PI * 2}
+    <Canvas camera={{ position: [-0.5, 1, 2] }}>
+      <Environment
+        files="./img/venice_sunset_1k.hdr"
+        background
+        backgroundBlurriness={0.1}
       />
-      <primitive
-        object={gltf.scene}
-        position={[0, 1, 0]}
-        children-0-castShadow
-      />
-      <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
-        <meshStandardMaterial />
-      </Circle>
-      <OrbitControls target={[0, 1, 0]} />
+      <directionalLight position={[3.3, 1.0, 4.4]} intensity={Math.PI * 2} />
+      <primitive object={gltf.scene} position={[0, 1, 0]} />
+      <OrbitControls target={[0, 1, 0]} autoRotate />
       <axesHelper args={[5]} />
       <Stats />
     </Canvas>
