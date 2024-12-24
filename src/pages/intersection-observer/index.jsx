2cb5875a04066e888a08dfcd5b9c8bd6b7b4769d
Goloviychuk Vadim <vadim.goloviychuk@gmail.com>
2024-12-24 15:58:13 +0200

    the intersection observer was updated

--- src/pages/intersection-observer/index.jsx
+++ src/pages/intersection-observer/index.jsx
@@ -1,14 +1,20 @@
 import { useRef, useEffect, useState, useCallback } from 'react';
-import { Flex, Text, Box } from '@chakra-ui/react';
+import { Flex, Text, Button, Grid } from '@chakra-ui/react';
 
 const useIntersectElementOnRoot = (options) => {
   const containerRef = useRef(null);
   const [isVisible, setVisible] = useState(false);
 
-  const callbackFn = useCallback((entries) => {
-    const [entry] = entries;
-    setVisible(entry.isIntersecting);
-  }, []);
+  const callbackFn = useCallback(
+    (entries) => {
+      const [entry] = entries;
+      setVisible(entry.isIntersecting);
+      if (entry.isIntersecting && isVisible) {
+        console.log('entry', entry);
+      }
+    },
+    [isVisible]
+  );
 
   useEffect(() => {
     const observer = new IntersectionObserver(callbackFn, options);
@@ -23,10 +29,11 @@ const useIntersectElementOnRoot = (options) => {
 };
 
 const IntersectionObserverPage = () => {
+  const [tracking, setTracking] = useState(0);
   const [containerRef, isVisible] = useIntersectElementOnRoot({
     root: null,
     rootMargin: '0px',
-    threshold: 1
+    threshold: tracking
   });
 
   return (
@@ -47,10 +54,19 @@ const IntersectionObserverPage = () => {
         width="full"
         padding="20px"
         bg="blue.100"
+        zIndex={1}
       >
         <Text>{isVisible ? 'In Viewport' : 'Not In Viewport'}</Text>
       </Flex>
-      <Box h="calc(100vh + 100px)" w="100%" />
+      <Flex direction="column" alignItems="center" h="calc(100vh + 100px)" w="100%" mt="70px">
+        <Text>Choose a variant of the tracking of the target box:</Text>
+        <Grid gap="15px" gridTemplateColumns="repeat(4, 1fr)" width="fit-content" mt="15px">
+          <Button onClick={() => setTracking(0)}>When the box appears</Button>
+          <Button onClick={() => setTracking(1)}>When the box appears fully</Button>
+          <Button onClick={() => setTracking([0.25, 0.5, 0.75, 1])}>By each 25% of the size</Button>
+          <Button onClick={() => setTracking([0.1, 0.3, 0.55, 0.85])}>Custom 10/30/55/85 %</Button>
+        </Grid>
+      </Flex>
       <Flex
         ref={containerRef}
         justifyContent="center"
