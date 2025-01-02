c1cae8543afb5a68b055b4abd1262cac14b95408
Goloviychuk Vadim <vadim.goloviychuk@gmail.com>
2025-01-02 21:34:24 +0200

    NextJS Essentials (App Router)

---
+++ components/meals/meals-grid.js
@@ -0,0 +1,10 @@
+import MealItem from './meail-item';
+import classes from './meal-grid.module.css';
+
+export default function MealsGrid({ meals }) {
+  return <ul className={classes.meals}>
+    {meals.map(meal => (<li key={meal.id}>
+      <MealItem {...meal}/>
+    </li>))}
+  </ul>
+}
